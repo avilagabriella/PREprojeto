e5e852a31195a1733698f81c9b54e7d4a341257b
mirellaxsj <mirellafrancasenai@gmail.com>
2024-12-11 10:36:43 -0300

    Atts

--- Mirella/back/src/controllers/usuarios.js
+++ Mirella/back/src/controllers/usuarios.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client')
+ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
