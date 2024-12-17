f6673b548b178a6212b5725e408529ed8ac226bf
BryanBeckham06 <beckham.souza06@eaportal.org>
2024-12-17 02:47:38 -0300

    Atts

--- projeto/escolhas/loja.js
+++ projeto/escolhas/loja.js
@@ -72,6 +72,7 @@ function addProductToCart(event) {
         <span class="cart-product-price">${productPrice}</span>
       </td>
       <td>
+  
         <input type="number" value="1" min="0" class="product-qtd-input">
         <button type="button" class="remove-product-button">Remover</button>
       </td>
